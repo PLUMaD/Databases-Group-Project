71023bea772b6cd26e20c213fdfa7e7e6c2c34e4
PLUMaD <91981771+PLUMaD@users.noreply.github.com>
2022-12-16 01:48:48 -0800

    Updated sql to build tables

--- .devcontainer/pg_init/01_create_tables.sql
+++ .devcontainer/pg_init/01_create_tables.sql
@@ -1,3 +1,58 @@
+create table animals {
+    animalsID int primary key,
+    animalsType text,
+    animalsOptionLeft text,
+    animalsCountLeft int,
+    animalsSubCategoryLeft text,
+    animalsOptionRight text,
+    animalsCountRight int,
+    animalsSubCategoryRight text
+};
+
+create table exotic {
+    exoticID int primary key,
+    exoticType text,
+    exoticOptionLeft text,
+    exoticCountLeft int,
+    exoticSubCategoryLeft text,
+    exoticOptionRight text,
+    exoticCountRight int,
+    exoticSubCategoryRight text
+};
+
+create table food {
+    foodID int primary key,
+    foodType text,
+    foodOptionLeft text,
+    foodCountLeft int,
+    foodSubCategoryLeft text,
+    foodOptionRight text,
+    foodCountRight int,
+    foodSubCategoryRight text
+};
+
+create table kittens {
+    kittiesID int primary key,
+    kittiesType text,
+    kittiesOptionLeft text,
+    kittiesCountLeft int,
+    kittiesSubCategoryLeft text,
+    kittiesOptionRight text,
+    kittiesCountRight int,
+    kittiesSubCategoryRight text
+};
+
+create table land {
+    landID int primary key,
+    landType text,
+    landOptionLeft text,
+    landCountLeft int,
+    landSubCategoryLeft text,
+    landOptionRight text,
+    landCountRight int,
+    landSubCategoryRight text
+};
+
 create table pets {
     petsID int primary key,
     petsType text,
@@ -9,7 +64,7 @@ create table pets {
     petsSubCategoryRight text
 };
 
-create table puppy {
+create table puppies {
     puppyID int primary key,
     puppyType text,
     puppyOptionLeft text,
@@ -18,4 +73,15 @@ create table puppy {
     puppyOptionRight text,
     puppyCountRight int,
     puppySubCategoryRight text
+};
+
+create table sea {
+    seaID int primary key,
+    seaType text,
+    seaOptionLeft text,
+    seaCountLeft int,
+    seaSubCategoryLeft text,
+    seaOptionRight text,
+    seaCountRight int,
+    seaSubCategoryRight text
 };
--- .devcontainer/pg_init/02_insert_rows.sql
+++ .devcontainer/pg_init/02_insert_rows.sql
@@ -1,3 +1,28 @@
+copy animals(animalsID, animalsType, animalsOptionLeft, animalsCountLeft, animalsSubCategoryLeft, animalsOptionRight, animalsCountRight, animalsSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/pets.csv'
+delimiter ','
+csv header;
+
+copy exotic(exoticID, exoticType, exoticOptionLeft, exoticCountLeft, exoticSubCategoryLeft, exoticOptionRight, exoticCountRight, exoticSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/puppies.csv'
+delimiter ','
+csv header;
+
+copy food(foodID, foodType, foodOptionLeft, foodCountLeft, foodSubCategoryLeft, foodOptionRight, foodCountRight, foodSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/pets.csv'
+delimiter ','
+csv header;
+
+copy kittens(kittiesID, kittiesType, kittiesOptionLeft, kittiesCountLeft, kittiesSubCategoryLeft, kittiesOptionRight, kittiesCountRight, kittiesSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/puppies.csv'
+delimiter ','
+csv header;
+
+copy land(landID, landType, landOptionLeft, landCountLeft, landSubCategoryLeft, landOptionRight, landCountRight, landSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/pets.csv'
+delimiter ','
+csv header;
+
 copy pets(petsID, petsType, petsOptionLeft, petsCountLeft, petsSubCategoryLeft, petsOptionRight, petsCountRight, petsSubCategoryRight)
 from '/docker-entrypoint-initdb.d/seed_data/pets.csv'
 delimiter ','
@@ -8,3 +33,7 @@ from '/docker-entrypoint-initdb.d/seed_data/puppies.csv'
 delimiter ','
 csv header;
 
+copy sea(seaID, seaType, seaOptionLeft, seaCountLeft, seaSubCategoryLeft, seaOptionRight, seaCountRight, seaSubCategoryRight)
+from '/docker-entrypoint-initdb.d/seed_data/sea.csv'
+delimiter ','
+csv header;
